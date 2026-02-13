70f7c354f30d525e464051c30538b8d1580601ac
glitchyxlatrell <latrellkong11@gmail.com>
2026-02-13 03:58:47 +0000

    arranging files for testing, and ready to start encrypting

--- hillplayfair.c
+++ hillplayfair.c
@@ -33,6 +33,11 @@ Due Date: February 16th 2026
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+
+void processPlaintext(char *plaintext, char *newText);
+void printWrapped(char *text);
 
 int main(int argc, char *argv[])
 {
@@ -45,31 +50,42 @@ int main(int argc, char *argv[])
 
   printf("\n");
 
+  // initializing files to open
+  FILE *hillKeyFile = NULL;
+  FILE *plaintextFile = NULL;
+  FILE *playfairKeyFile = NULL;
+
+  // checking if all arguments were provided
   if (argc == 5)
   {
+    // arg 1 has to be the command encrypt
     if (strcmp(argv[1], "encrypt") != 0)
     {
       return 1;
     }
 
-    FILE *hillKeyFile = fopen(argv[2], "r");
+    // printing mode if encryption is present
+    printf("Mode: \nEncryption Mode\n");
+
+    // opening all files to read
+    hillKeyFile = fopen(argv[2], "r");
     if (hillKeyFile == NULL)
     {
-      printf("key file could not be opened");
+      printf("key file could not be opened\n");
       return 1;
     }
 
-    FILE *plaintextFile = fopen(argv[3], "r");
+    plaintextFile = fopen(argv[3], "r");
     if (plaintextFile == NULL)
     {
-      printf("plaintext file could not be opened");
+      printf("plaintext file could not be opened\n");
       return 1;
     }
 
-    FILE *playfairKeyFile = fopen(argv[4], "r");
+    playfairKeyFile = fopen(argv[4], "r");
     if (playfairKeyFile == NULL)
     {
-      printf("playfair key file could not be opened");
+      printf("playfair key file could not be opened\n");
       return 1;
     }
   }
@@ -78,16 +94,92 @@ int main(int argc, char *argv[])
   {
     printf("Invalid number of arguments provided.\n");
     printf("Try: ./00_args 123\n");
+    return 1;
   }
 
-  
+  // opening hill key file for the matrix
+  int nHill = 0;
+  if(fscanf(hillKeyFile,"%d", &nHill) != 1  || nHill < 2 || nHill > 9)
+  {
+    printf("invalid n value in hill key file\n");
+    return 1;
+  }
 
+  // parsing through file to fill out matrix key
+  int hillMatrix[nHill][nHill];
+  for(int i = 0; i < nHill; i++)
+  {
+    for(int j = 0; j < nHill; j++)
+    {
+      if(fscanf(hillKeyFile, "%d", &hillMatrix[i][j]) != 1)
+      {
+        printf("invalid matrix in hill key file\n");
+        return 1;
+      }
+    }
+  }
 
+  // variables to take in plaintext
+  char plaintext[10000];
+  int temp = 0;
+  int tempChar;
 
+  // parsing through plaintext file using fgetc for every character
+  while ((tempChar = fgetc(plaintextFile)) != EOF)
+  {
+    plaintext[temp] = (char)tempChar;
+    temp++;
+  }
+  plaintext[temp] = '\0';
 
+  char playfairKey[100];
+  if(fscanf(playfairKeyFile, "%99s", playfairKey) != 1)
+  {
+    printf("error getting playfair key");
+  }
+
+  printf("\nOriginal Plaintext:\n%s\n", plaintext);
+
+  char preprocessPlaintext[10000];
+  processPlaintext(plaintext, preprocessPlaintext);
+
+  printf("Preprocessed Plaintext:\n");
+  printWrapped(preprocessPlaintext);
 
 
 
 
   return 0;
+}
+
+void processPlaintext(char *plaintext, char *newText)
+{
+  int counter = 0;
+  for(int i = 0; plaintext[i] != '\0'; i++)
+  {
+    if((plaintext[i] >= 'A' && plaintext[i] <= 'Z') || (plaintext[i] >= 'a' && plaintext[i] <= 'z'))
+    {
+      newText[counter] = (char)toupper(plaintext[i]);
+      counter++;
+    }
+  }
+  newText[counter] = '\0';
+  return;
+}
+
+void printWrapped(char *text)
+{
+  int counter = 0;
+  for(int i = 0; text[i] != '\0'; i++)
+  {
+    printf("%c ",text[i]);
+    counter++;
+
+    if(counter == 80)
+    {
+      printf("\n");
+      counter = 0;
+    }
+  }
+  printf("\n");
 }
